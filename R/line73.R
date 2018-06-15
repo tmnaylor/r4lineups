2c9065afeef7e73f7f84d142481d6f1bcd0d69af
Tamsyn Naylor <tmnaylor@outlook.com>
2018-06-15 14:28:15 +0200

    Version 0.1.1

---
+++ R/line73.R
@@ -0,0 +1,15 @@
+#'line73
+#'@name line73
+#'@docType data
+#'@usage data(line73)
+#'@description A dataframe of lineup choices for 1 trial; real data from a Doob & Kirshenbaum (1973) article.
+#'             This article was the first to discuss lineup size
+#'             1 column x 42 observations
+#'
+#'             42 mock witnesses
+#'             Nominal size is 6
+#'             Target position is 4
+#'
+#'@references Doob, A. N., & Kirshenbaum, H. M. (1973). Bias in police lineups-Partial remembering.
+#'        \emph{Journal of Police Science and Administration, 1}, 287-293.
+"line73"
---
+++ R/mockdata.R
@@ -0,0 +1,27 @@
+#'mockdata
+#'@name mockdata
+#'@docType data
+#'@usage data(mockdata)
+#'@description  A dataframe containing real lineup data.
+#'
+#'              3 columns x 94 observations.
+#'
+#'              The first column simply provides the participant number, whilst
+#'              the second and third contain data for each participant's choice (i.e., the position of the
+#'              lineup member that they identified) and a confidence score for each choice.
+#'
+#'              In the field of eyewitness testimony, it is common for participants to rate how confident
+#'              they are that they have accurately identified the perpetrator.
+#'              Here, confidence is recorded as a percentage.
+#'
+#'              This lineup has the following parameters:
+#'
+#'              \itemize{
+#'              \item 94 mock witness choices
+#'
+#'              \item Target position is 7
+#'
+#'              \item Nominal size is 9
+#'              }
+#'
+"mockdata"
