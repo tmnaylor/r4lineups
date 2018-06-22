a9bc315cce6c628cb97f65ebb990d7d9205cac4b
Tamsyn Naylor <tmnaylor@outlook.com>
2018-06-22 13:41:00 +0200

    Version 0.1.1

---
+++ R/malpass_foils.R
@@ -0,0 +1,10 @@
+#'malpass_foils
+#'@name malpass_foils
+#'@docType data
+#'@usage data(malpass_foils)
+#'@description A list object containing data for images of 6 members of a lineup - add
+#'             Images stored as a list. Each image can be reconstructed by calling 'image_write' (from package
+#'             'magick) on each object in the list.
+#'
+#'
+"malpass_foils"
